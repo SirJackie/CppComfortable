d39a900ffbde8d99f2605d45400be78b839e9227
lyj00912 <3122742760@qq.com>
2022-01-03 14:55:39 +0800

    made mouse msg

--- main.cpp
+++ main.cpp
@@ -1,19 +1,43 @@
+//用户交互——鼠标2
 #include <graphics.h>
+
 #include <stdio.h>
+
 int main()
 {
 	initgraph(640, 480);
-	outtextxy(0, 0, "please press any key");
 	
-	int k;
-	for (;;)
+	setrendermode(RENDER_MANUAL); //不使用自动刷新，减少闪烁 
+	
+	setfont(18, 0, "宋体");
+	
+	mouse_msg msg = {0};
+	for ( ; is_run(); delay_fps(60))
 	{
-		if ( kbhit() ){
-			k = getch();
-			cleardevice();
+		//获取鼠标消息，此函数不会等待，运行后会立即返回
+		while (mousemsg())
+		{
+			// 也可以用简单形式：mousepos(&x, &y);
+			msg = getmouse();
 		}
+		
+		cleardevice();
+		xyprintf(0, 0, "x = %10d  y = %10d",
+			msg.x, msg.y);
+		xyprintf(0, 20, "move  = %d down  = %d up    = %d",
+			(int)msg.is_move(),
+			(int)msg.is_down(),
+			(int)msg.is_up());
+		xyprintf(0, 40, "left  = %d mid   = %d right = %d",
+			(int)msg.is_left(),
+			(int)msg.is_mid(),
+			(int)msg.is_right());
+		xyprintf(0, 60, "wheel = %d  wheel rotate = %d",
+			(int)msg.is_wheel(),
+			msg.wheel);
 	}
 	
 	closegraph();
+	return 0;
 }
 
