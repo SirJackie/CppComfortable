4d2d410c32ebdd932503518e35e678c4025cfdb8
lyj00912 <3122742760@qq.com>
2022-01-08 10:55:29 +0800

    replaced settarget() using setvisualpage() and setactivepage(), the the front buffer could be properly changed

--- main.cpp
+++ main.cpp
@@ -6,43 +6,33 @@ using namespace std;
 #define WIDTH 1024
 #define HEIGHT 576
 
-void setup(PIMAGE& image, char* buffer){
-	setfillcolor(RED, image);
-	bar(10, 10, 100, 100, image);
+void ShowPtrAddr(void* ptr){
+	cout << (unsigned long long) ptr << endl;
 }
 
-void loop (PIMAGE& image, char* buffer){
-	cout<<(unsigned long long)((void*)buffer)<<endl;
-//	setfillcolor(RED, image);
-//	bar(20, 20, 110, 110, image);
+void setup(PIMAGE image){
+	setfillcolor(RED);
+	bar(10, 10, 100, 100);
+}
+
+void loop (PIMAGE image){
+	;
 }
 
 int main() {
 
 	initgraph(WIDTH, HEIGHT, INIT_RENDERMANUAL);
+	
+	int frontBuffer = 0;
 
-	PIMAGE buffer1 = gettarget();
-	PIMAGE buffer2 = newimage(WIDTH, HEIGHT);
-
-	PIMAGE* frontBuffer = &buffer1;
-	PIMAGE* backBuffer  = &buffer2;
-	PIMAGE* tmpBufPtr = nullptr;
-
-	setup( (*backBuffer), (char*)getbuffer((*backBuffer)) );
-
-	// Swap Buffer
-	tmpBufPtr = backBuffer;
-	backBuffer = frontBuffer;
-	frontBuffer = tmpBufPtr;
-
-	for( ; is_run(); delay_fps(60) ) {
-		loop( (*backBuffer), (char*)getbuffer((*backBuffer)) );
-		settarget(*backBuffer);
-
-		// Swap Buffer
-		tmpBufPtr = backBuffer;
-		backBuffer = frontBuffer;
-		frontBuffer = tmpBufPtr;
+	for( ; is_run(); delay_fps(2) ) {
+		setvisualpage(frontBuffer);
+		setactivepage(!frontBuffer);
+		frontBuffer = !frontBuffer;
+		cout<<frontBuffer<<endl;
+		if(frontBuffer == 0){
+			setup(gettarget());
+		}
 	}
 
 	closegraph();
