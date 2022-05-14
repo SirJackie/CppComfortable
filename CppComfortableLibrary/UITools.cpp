07ad37dcbc62bc027d990dfc702157af960bca4f
lyj00912 <3122742760@qq.com>
2022-05-14 10:38:19 +0800

    created 45-90deg adaption for DrawBresenhamLine()

--- CppComfortableLibrary/UITools.cpp
+++ CppComfortableLibrary/UITools.cpp
@@ -2,13 +2,25 @@
 
 void DrawBresenhamLine(int x0, int y0, int x1, int y1, int color){
 	
+	// Crucial Point: 45 - 90 deg adapter
+	bool steep = abs(y1 - y0) > abs(x1 - x0);
+	if(steep){
+		swap(&x1, &y1);
+		swap(&x0, &y0);
+	}
+	
 	int dx = x1 - x0;
 	int dy = y1 - y0;
 	int y = y0;
 	int pk = 2 * dy - dx;  // Init value of decision parameter
 	
 	for(int x = x0; x < x1; x++){
-		SetPixel(x, y, color);
+		if(steep){
+			SetPixel(y, x, color);
+		}
+		else{
+			SetPixel(x, y, color);
+		}
 		
 		if(pk < 0){        // When pk < 0
 			pk += 2 * dy;  // pk+1 = pk + 2dy
